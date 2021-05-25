b9b80cdc7f87cbb79760b610ad35807ce68e7663
gmartpad <gabrielmpwork@gmail.com>
2021-05-25 20:31:34 -0300

    feature: installed dotenv for environment variables and configured db connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const customExpress = require('./config/customExpress')
 
+// initializando a utilização de variáveis de ambiente
+require('dotenv').config();
+
 // instanciando o servidor com as configs customizadas
 const app = customExpress();
 
 // porta no qual o servidor estará escutando
-const port = 5000;
+const port = process.env.PORT;
 
 // O servidor estará rodando e escutando na porta 5000
 app.listen(port, () => {
