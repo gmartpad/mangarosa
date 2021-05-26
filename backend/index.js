31e0a6144e4f88d12da1df8be6eaeb03a0c324d5
gmartpad <gabrielmpwork@gmail.com>
2021-05-25 22:20:26 -0300

    feature: fixed SQLs for table creation and fixed dotenv require error

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,35 @@
-const customExpress = require('./config/customExpress')
-
-// initializando a utilização de variáveis de ambiente
 require('dotenv').config();
 
-// instanciando o servidor com as configs customizadas
-const app = customExpress();
+const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
-// porta no qual o servidor estará escutando
-const port = process.env.PORT;
+// initializando a utilização de variáveis de ambiente
 
-// O servidor estará rodando e escutando na porta 5000
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-});
+conexao.connect(err => {
+
+    if(err) {
+
+        console.log(err)
+
+    } else {
+
+
+        console.log(`Conexão ao banco de dados realizada com sucesso!`)
+
+        Tabelas.init(conexao);
+
+        // instanciando o servidor com as configs customizadas
+        const app = customExpress();
+
+        // porta no qual o servidor estará escutando
+        const port = process.env.PORT;
+
+        // O servidor estará rodando e escutando na porta 5000
+        app.listen(port, () => {
+            console.log(`Servidor rodando na porta ${port}`);
+        });
+
+    }
+    
+})
--- backend/infraestrutura/tabelas.js
+++ backend/infraestrutura/tabelas.js
@@ -8,11 +8,12 @@ class Tabelas {
     }
 
     criarUsuarios() {
-        const sql = `CREATE TABLE IF NOT EXISTS Usuarios (id int NOT NULL AUTO_INCREMENT, usuario varchar(100) NOT NULL, nome varchar(100) NOT NULL, senha char(60) NOT NULL, email varchar(100) NOT NULL, cargo char(1) NOT NULL, PRIMARY KEY(id))`;
+        const sql = `CREATE TABLE IF NOT EXISTS registrodeponto.Usuarios (id_usuario int NOT NULL, usuario varchar(100) NOT NULL, nome varchar(100) NOT NULL, senha char(60) NOT NULL, email varchar(100) NOT NULL, cargo char(1) NOT NULL, PRIMARY KEY(id_usuario), UNIQUE(id_usuario))`;
 
         this.conexao.query(sql, err => {
             if(err) {
                 console.log(`Houve falha na execução do SQL de criação da tabela Usuarios`);
+                console.log(err);
             } else {
                 console.log(`Criação da tabela Usuarios realizada com sucesso!`);
             }
@@ -20,11 +21,12 @@ class Tabelas {
     }
 
     criarRegistros() {
-        const sql = `CREATE TABLE IF NOT EXISTS Registros (id int NOT NULL AUTO_INCREMENT, idNRegistroUsuario int NOT NULL, usuario varchar(100), nome varchar(100) NOT NULL, email varchar(100) NOT NULL, cpf char(14) NOT NULL, celular varchar(11), conhecimentos varchar(40), dateTimeRegistro datetime NOT NULL, validado char(1) NOT NULL DEFAULT 'N', dateTimeValidacao datetime, PRIMARY KEY(id), FOREIGN KEY (usuario) REFERENCES Usuarios(usuario))`;
+        const sql = `CREATE TABLE IF NOT EXISTS registrodeponto.Registros (id_registro int NOT NULL, idNRegistroUsuario int NOT NULL, usuario int, nome varchar(100) NOT NULL, email varchar(100) NOT NULL, cpf char(14) NOT NULL, celular varchar(11), conhecimentos varchar(40), dateTimeRegistro timestamp NOT NULL, validado char(1) NOT NULL DEFAULT 'N', dateTimeValidacao timestamp, PRIMARY KEY(id_registro), UNIQUE(id_registro), UNIQUE(cpf), CONSTRAINT fk_usuario FOREIGN KEY (usuario) REFERENCES registrodeponto.Usuarios(id_usuario))`;
 
         this.conexao.query(sql, err => {
             if(err) {
                 console.log(`Houve falha na execução no SQL de criação da tabela Registros`);
+                console.log(err);
             } else {
                 console.log(`Criação da tabela Registros realizada com sucesso!`);
             }
