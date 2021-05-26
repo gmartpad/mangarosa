1d1accf0d87f7bb0b20f72591cd9f157c2957223
gmartpad <gabrielmpwork@gmail.com>
2021-05-26 05:04:09 -0300

    feature: installed sqlutils and added the other API REST methods for Usuarios

--- backend/controllers/usuarios.js
+++ backend/controllers/usuarios.js
@@ -1,6 +1,17 @@
 const Usuario = require('../models/usuarios');
 
 module.exports = app => {
+
+    app.get('/usuarios', (req, res) => {
+        Usuario.lista(res);
+    })
+
+    app.get('/usuarios/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+
+        Usuario.buscaPorId(id, res);
+    })
+
     app.post('/usuarios', (req, res) => {
 
         const usuario = req.body;
@@ -8,4 +19,17 @@ module.exports = app => {
         Usuario.adiciona(usuario, res);
 
     })
+
+    app.patch('/usuarios/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        const valores = req.body;
+
+        Usuario.altera(id, valores, res);
+    })
+
+    app.delete('/usuarios/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        Usuario.deleta(id, res);
+    })
+
 }
--- backend/models/usuarios.js
+++ backend/models/usuarios.js
@@ -1,6 +1,7 @@
 const moment = require('moment');
 const conexao = require('../infraestrutura/conexao');
 const bcrypt = require('bcrypt');
+const format = require('sqlutils/pg/format');
 
 const removeAcento = require('../utils/removeAcento');
 
@@ -47,8 +48,10 @@ class Usuario {
                    usuario: nomeFormatado,
                    ...usuario,
                 }
+
+                const sql = format('INSERT INTO registrodeponto.Usuarios ?', usuarioF);
     
-                const sql = 'INSERT INTO registrodeponto.Usuarios (usuario, nome, senha, email, cargo) VALUES (' + `'` + [usuarioF.usuario, usuarioF.nome, usuarioF.senha, usuarioF.email, usuarioF.cargo ].join(`','`) + `'` + ')';
+                // const sql = 'INSERT INTO registrodeponto.Usuarios (usuario, nome, senha, email, cargo) VALUES (' + `'` + [usuarioF.usuario, usuarioF.nome, usuarioF.senha, usuarioF.email, usuarioF.cargo ].join(`','`) + `'` + ')';
 
                 conexao.query(sql, (erro, resultados) => {
                     if(erro){
@@ -61,6 +64,60 @@ class Usuario {
             });
 
         }
+    }
+
+    lista(res) {
+        const sql = "SELECT * FROM registrodeponto.Usuarios";
+
+        conexao.query(sql, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                if(resultados.rows.length >= 2){
+                    res.status(201).json(resultados.rows)
+                } else {
+                    res.status(201).json(...resultados.rows)
+                }
+            }
+        })
+    }
+
+    buscaPorId(id, res) {
+        const sql = `SELECT * FROM registrodeponto.Usuarios WHERE id_usuario=$1`;
+
+        conexao.query(sql, [id], (erro, resultados) => {
+            // const usuario = resultados[0]
+            if(erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(201).json(...resultados.rows);
+            }
+        })
+    }
+
+    altera(id, valores, res) {
+
+        const sql = format(`UPDATE registrodeponto.Usuarios SET ? WHERE id_usuario=${id}`, valores);
+
+        conexao.query(sql, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(201).json({...valores, id});
+            }
+        })
+    }
+
+    deleta(id, res) {
+        const sql = `DELETE FROM registrodeponto.Usuarios WHERE id_usuario=${id}`;
+
+        conexao.query(sql, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro.message);
+            } else {
+                res.status(201).json({id});
+            }
+        })
     }
 
 }
